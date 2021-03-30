8dd4bff12ee697cd320a1a8082ff6f899a17d616
Skriaga <skriaga.py@gmail.com>
2021-03-31 01:11:25 +0300

    hw6

--- lesson_6/2.py
+++ lesson_6/2.py
@@ -21,3 +21,25 @@ nginx_logs/nginx_logs) — получить список кортежей вид
 Примечания: спамер — это клиент, отправивший больше всех запросов; код должен
 работать даже с файлами, размер которых превышает объем ОЗУ компьютера.
 '''
+
+from urllib import request as req
+from collections import Counter
+
+with req.urlopen('https://github.com/elastic/examples/raw/master/'
+                 'Common%20Data%20Formats/nginx_logs/nginx_logs') as file:
+    base = [line.split()[0] for line in file]
+
+ip, count = Counter(base).most_common(1)[0]
+print(f'IP адрес спамера: {ip.decode("utf-8")}, количество запросов: {count}')
+
+
+
+
+
+
+
+
+
+
+
+
--- lesson_6/5.py
+++ lesson_6/5.py
@@ -31,3 +31,16 @@
 чтобы можно было задать имя обоих исходных файлов и имя выходного файла.
 Проверить работу скрипта.
 '''
+
+from sys import argv
+
+with open(argv[1], 'r', encoding='utf-8') as users, \
+        open(argv[2], 'r', encoding='utf-8') as hobby, \
+        open(argv[3], 'a', encoding='utf-8') as users_hobby:
+    for usr, hob in zip(users, hobby):
+        users_hobby.write(usr.replace("\n", "") + ': ' + hob)
+
+
+
+
+
