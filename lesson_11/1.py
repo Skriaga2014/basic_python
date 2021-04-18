db77eba259f7aa4f7fe2f330b726a37195e45c5f
Skriaga <skriaga.py@gmail.com>
2021-04-18 12:31:41 +0300

    lesson_11_task_1

--- lesson_11/1.py
+++ lesson_11/1.py
@@ -8,3 +8,33 @@
 на реальных данных.
 '''
 
+import datetime
+import re
+
+class Date:
+
+    def __init__(self, date):
+        self.date = date
+
+    @classmethod
+    def method1(cls, date):
+        pattern = re.compile(r'((?P<day>\d+)\W(?P<month>\d+)\W(?P<year>\d+))')
+        dt = pattern.search(date)
+
+        return int(dt['day']), int(dt['month']), int(dt['year'])
+
+    @staticmethod
+    def method2(nums):
+        nums = tuple(reversed(nums))
+        try:
+            result_date = datetime.date(*nums)
+        except ValueError as error:
+            quit(error)
+        return result_date
+
+
+d1 = Date('10-11-2021')
+date_tuple = Date.method1(d1.date)
+print(date_tuple)
+date = Date.method2(date_tuple)
+print(date)
